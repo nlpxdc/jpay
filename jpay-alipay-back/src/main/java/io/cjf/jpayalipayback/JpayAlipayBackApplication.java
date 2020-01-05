1bcde0daa8ee548904ada164f5653521326eee33
chenjiefei <nlpx_dc@hotmail.com>
2020-01-05 11:57:58 +0800

    first init alipay back

---
+++ jpay-alipay-back/src/main/java/io/cjf/jpayalipayback/JpayAlipayBackApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jpayalipayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpayAlipayBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpayAlipayBackApplication.class, args);
+    }
+
+}
---
+++ jpay-alipay-back/src/test/java/io/cjf/jpayalipayback/JpayAlipayBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cjf.jpayalipayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpayAlipayBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
