2e62bd3b3ac565511be2aa9535b51adf2a5de410
chenjiefei <nlpx_dc@hotmail.com>
2020-01-05 15:17:40 +0800

    first init alipay notify service

---
+++ jpay-alipay-notify/src/main/java/io/cjf/jpayalipaynotify/JpayAlipayNotifyApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jpayalipaynotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpayAlipayNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpayAlipayNotifyApplication.class, args);
+    }
+
+}
---
+++ jpay-alipay-notify/src/test/java/io/cjf/jpayalipaynotify/JpayAlipayNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cjf.jpayalipaynotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpayAlipayNotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
