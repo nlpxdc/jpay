0f472289fa5adc2f9049386bafcabc14555f9f33
chenjiefei <nlpx_dc@hotmail.com>
2020-01-05 17:42:44 +0800

    draft order pay page

--- jpay-alipay-front/viewmodels/order-pay.js
+++ jpay-alipay-front/viewmodels/order-pay.js
@@ -1,9 +1,21 @@
 var app = new Vue({
     el: '#app',
-    data: {},
+    data: {
+    },
+    mounted() {
+        console.log('view mounted');
+        this.getOrderPayPage();
+    },
     methods: {
         getOrderPayPage() {
-            
+            axios.get('/order/getOrderPayPage')
+                .then(function (response) {
+                    console.log(response);
+                    document.write(response.data);
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
         }
     }
 });
