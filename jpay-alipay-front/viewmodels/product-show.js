4c5d9d0056b4fd6e357bff5a073c2f25d8cf3ad0
chenjiefei <nlpx_dc@hotmail.com>
2020-01-05 17:47:37 +0800

    product to pay page

--- jpay-alipay-front/viewmodels/product-show.js
+++ jpay-alipay-front/viewmodels/product-show.js
@@ -1,9 +1,20 @@
 var app = new Vue({
     el: '#app',
     data: {},
-    methods:{
-        handleBuyClick(){
+    methods: {
+        handleBuyClick() {
             console.log('buy click');
+            this.getOrderPayPage();
+        },
+        getOrderPayPage() {
+            axios.get('/order/getOrderPayPage')
+                .then(function (response) {
+                    console.log(response);
+                    document.write(response.data);
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
         }
     }
 })
