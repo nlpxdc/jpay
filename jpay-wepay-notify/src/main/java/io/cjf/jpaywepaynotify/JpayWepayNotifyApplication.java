147d89445b40ca8e7925af83428bafda8526d930
chenjiefei <nlpx_dc@hotmail.com>
2020-01-13 20:42:43 +0800

    first init wepay notify

---
+++ jpay-wepay-notify/src/main/java/io/cjf/jpaywepaynotify/JpayWepayNotifyApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jpaywepaynotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpayWepayNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpayWepayNotifyApplication.class, args);
+    }
+
+}
---
+++ jpay-wepay-notify/src/test/java/io/cjf/jpaywepaynotify/JpayWepayNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cjf.jpaywepaynotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpayWepayNotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
