0666fcfc7254bc8036c0c1b6e02fabf6fc52890a
chenjiefei <nlpx_dc@hotmail.com>
2020-01-13 18:45:01 +0800

    first init wepay back

---
+++ jpay-wepay-back/src/main/java/io/cjf/jpaywepayback/JpayWepayBackApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jpaywepayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpayWepayBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpayWepayBackApplication.class, args);
+    }
+
+}
---
+++ jpay-wepay-back/src/test/java/io/cjf/jpaywepayback/JpayWepayBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cjf.jpaywepayback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpayWepayBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
