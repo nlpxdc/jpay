5f65895d790dfc1326d97ec097c8cf869491d562
chenjiefei <nlpx_dc@hotmail.com>
2020-01-21 22:01:17 +0800

    add qrcode page

--- jpay-alipay-front/viewmodels/product-show.js
+++ jpay-alipay-front/viewmodels/product-show.js
@@ -1,7 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
-        amount: ''
+        amount: 0.01,
+        codeUrl: '',
+        qrcode: ''
+    },
+    mounted() {
+        console.log('view mounted');
+        this.qrcode = new QRCode(document.getElementById("qrcode"));
     },
     methods: {
         handleBuyClick() {
@@ -11,6 +17,31 @@ var app = new Vue({
         handleMobileBuyClick() {
             console.log('mobile buy click');
             location.href = 'http://192.168.137.1/jpayalipayback/order/getOrderPayWap?amount=' + this.amount;
+        },
+        handleGeneratePayQRCodeClick() {
+            console.log('generate pay qrcode click');
+            this.getPayQRCode();
+        },
+        getPayQRCode() {
+            axios.get('/f2f/getOrderPayQRCode', {
+                params: {
+                    amount: this.amount
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.codeUrl = response.data;
+                    if (app.codeUrl) {
+                        app.qrcode.makeCode(app.codeUrl);
+                    }else{
+                        // app.qrcode.clear();
+                        alert('二维码获取失败');
+                    }
+                    
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
         }
     }
 })
