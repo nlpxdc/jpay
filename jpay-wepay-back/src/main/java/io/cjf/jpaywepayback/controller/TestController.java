45b992c3a9809129508c719c340a685230430959
chenjiefei <nlpx_dc@hotmail.com>
2020-01-21 12:34:53 +0800

    add json xml

--- jpay-wepay-back/src/main/java/io/cjf/jpaywepayback/controller/TestController.java
+++ jpay-wepay-back/src/main/java/io/cjf/jpaywepayback/controller/TestController.java
@@ -1,6 +1,7 @@
 package io.cjf.jpaywepayback.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import io.cjf.jpaywepayback.dto.MyJson;
 import io.cjf.jpaywepayback.dto.PayUnifiedOrderDTO;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,14 @@ public class TestController {
 //        xml.put("bb", "bbvv");
         PayUnifiedOrderDTO payUnifiedOrderDTO = new PayUnifiedOrderDTO();
         return payUnifiedOrderDTO;
+    }
+
+    @GetMapping(value = "/myJson", produces = MediaType.APPLICATION_XML_VALUE)
+    public MyJson myJson(){
+        final MyJson myJson = new MyJson();
+        myJson.put("aa", "aav");
+        myJson.put("bb", "bbv");
+        return myJson;
     }
 
 }
---
+++ jpay-wepay-back/src/main/java/io/cjf/jpaywepayback/dto/MyJson.java
@@ -0,0 +1,8 @@
+package io.cjf.jpaywepayback.dto;
+
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+@JacksonXmlRootElement(localName = "xml")
+public class MyJson extends JSONObject {
+}
